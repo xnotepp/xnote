0005cfb46ade4d53734774cb3bb9cedbfe16dcee
Klaus Buecher <buecher@optosolar.com>
2020-10-09 17:14:33 +0200

    some TODOs

--- src/xn-background.js
+++ src/xn-background.js
@@ -3,6 +3,15 @@
  * https://github.com/thundernest/addon-developer-support/wiki/Using-the-WindowListener-API-to-convert-a-Legacy-Overlay-WebExtension-into-a-MailExtension-for-Thunderbird-78
  */
 
+//TODO
+/*
+all locale files
+x   note does not close
+preferences
+displaying a new note by click triggers unload listener (ca. 6 times)
+*/
+
+
 async function main() {
 
     
@@ -13,58 +22,19 @@ async function main() {
  
         ["locale", "xnote", "en-US", "chrome/locale/en-US/"],
         ["locale", "xnote", "de", "chrome/locale/de/"],
-        /*
-        ["locale", "quickfolders", "ca", "chrome/locale/ca/"],
-        ["locale", "quickfolders", "es-MX", "chrome/locale/es-MX/"],
-        ["locale", "quickfolders", "es", "chrome/locale/es/"],
-        ["locale", "quickfolders", "fr", "chrome/locale/fr/"],
-        ["locale", "quickfolders", "hu-HU", "chrome/locale/hu-HU/"],
-        ["locale", "quickfolders", "it", "chrome/locale/it/"],
-        ["locale", "quickfolders", "ja-JP", "chrome/locale/ja-JP/"],
-        ["locale", "quickfolders", "nl", "chrome/locale/nl/"],
-        ["locale", "quickfolders", "pl", "chrome/locale/pl/"],
-        ["locale", "quickfolders", "pt-BR", "chrome/locale/pt-BR/"],
-        ["locale", "quickfolders", "ru", "chrome/locale/ru/"],
-        ["locale", "quickfolders", "sl-SI", "chrome/locale/sl-SI/"],
-        ["locale", "quickfolders", "sr", "chrome/locale/sr/"],
-        ["locale", "quickfolders", "sv-SE", "chrome/locale/sv-SE/"],
-        ["locale", "quickfolders", "vi", "chrome/locale/vi/"],
-        ["locale", "quickfolders", "zh-CN", "chrome/locale/zh-CN/"],
-        ["locale", "quickfolders", "zh-CHS", "chrome/locale/zh-CN/"],
-        ["locale", "quickfolders", "zh", "chrome/locale/zh/"],
-        ["locale", "quickfolders", "zh-CHT", "chrome/locale/zh/"],
-        ["locale", "quickfolders", "zh-TW", "chrome/locale/zh/"]
-    */
       ]);
  
- //   messenger.WindowListener.registerOptionsPage("chrome://quickfolders/content/options.xhtml"); 
+ //   messenger.WindowListener.registerOptionsPage("chrome://xnote/content/options.xhtml"); 
     
- //attention: each target window (like messenger.xul) can appear only once
- // this is different from chrome.manifest
- // xhtml for Tb78
-    // messenger.WindowListener.registerWindow("chrome://messenger/content/messenger.xhtml", "chrome/content/scripts/qf-messenger.js");
-    /* not necessary in Tb78+    */
  
 
     messenger.WindowListener.registerDefaultPrefs("chrome/content/scripts/xn-defaultPrefs.js");
 
     messenger.WindowListener.registerWindow("chrome://messenger/content/messenger.xul", "chrome/content/scripts/xn-xnote-overlay.js");
     messenger.WindowListener.registerWindow("chrome://messenger/content/messenger.xhtml", "chrome/content/scripts/xn-xnote-overlay.js");
-    /*   messenger.WindowListener.registerWindow("chrome://messenger/content/messengercompose.xul", "chrome/content/scripts/qf-composer.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/FilterListDialog.xul", "chrome/content/scripts/qf-filterlist.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/SearchDialog.xul", "chrome/content/scripts/qf-searchDialog.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/customizeToolbar.xul", "chrome/content/scripts/qf-customizetoolbar.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/messageWindow.xul", "chrome/content/scripts/qf-messageWindow.js");
-    
-    messenger.WindowListener.registerWindow("chrome://messenger/content/messenger.xhtml", "chrome/content/scripts/qf-messenger.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/messengercompose.xhtml", "chrome/content/scripts/qf-composer.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/FilterListDialog.xhtml", "chrome/content/scripts/qf-filterlist.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/SearchDialog.xhtml", "chrome/content/scripts/qf-searchDialog.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/customizeToolbar.xhtml", "chrome/content/scripts/qf-customizetoolbar.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/messageWindow.xhtml", "chrome/content/scripts/qf-messageWindow.js");  
-  */
-    messenger.WindowListener.registerStartupScript("chrome/content/scripts/qf-startup.js");
-    messenger.WindowListener.registerShutdownScript("chrome/content/scripts/qf-shutdown.js");
+   
+   // messenger.WindowListener.registerStartupScript("chrome/content/scripts/xn-startup.js");
+  //  messenger.WindowListener.registerShutdownScript("chrome/content/scripts/xn-shutdown.js");
 
  /*
   * Start listening for opened windows. Whenever a window is opened, the registered
